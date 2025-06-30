3bc0975cdcab0430fd6ecdf228824595f7c022be
MarcinMinkowski <minkowski.m@gmail.com>
2025-06-30 15:13:04 +0300

    Training PINN

--- PINN/dataset.py
+++ PINN/dataset.py
@@ -2,11 +2,13 @@ import os
 import torch
 from torch.utils.data import Dataset
 import pandas as pd
+from scipy.constants import pi
 
 class DriftwaveDataset(Dataset):
     def __init__(self):
         self.list = [val for sublist in [[os.path.join(root,name) for name in file] for root, dir, file in os.walk("../NewDataset/Ln0.01Ky1")] for val in sublist]
         self.resolution = 100
+        self.kygrid = torch.linspace(0,2*pi,self.resolution)
 
     def __len__(self):
         return len(self.list)*self.resolution
@@ -14,10 +16,9 @@ class DriftwaveDataset(Dataset):
     def __getitem__(self, index):
         df = pd.read_csv(self.list[int(index/self.resolution)])
         
-        #Ky = df.iloc[0].loc['Ky']
-        #Ln = df.iloc[0].loc['Ln']
+        y = self.kygrid[index%self.resolution]
         Ts = df.iloc[0].loc['Ts']
 
         sol = torch.tensor(df['sol']/1e17)[index%self.resolution]
 
-        return torch.tensor([index%self.resolution,Ts]).float(), sol.float()
+        return torch.tensor([y,Ts]).float(), sol.float()
---
+++ PINN/trainPINN.py
@@ -0,0 +1,62 @@
+from PINN import PINN
+from dataset import DriftwaveDataset
+import torch
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
+from scipy.constants import pi
+
+def train_loop(dataloader, model, loss_fn, optimizer, batch_size, v):
+    num_batches = len(dataloader)
+    total_loss_data = total_loss_pinn = 0.0
+    for (X,y) in dataloader:
+        X = X.to(device)
+        y = y.to(device)
+
+        pred = model(X)
+        loss_data = loss_fn(pred,y.unsqueeze(-1))
+
+        derivatives = torch.autograd.functional.jacobian(model,X)
+        derivatives = torch.sum(derivatives,2)
+        dndy = derivatives[:,:,0]
+        dndt = derivatives[:,:,1]
+        loss_pinn = loss_fn(-v*dndy,dndt)
+
+        loss = loss_data + loss_pinn
+        
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        total_loss_data = total_loss_data + loss_data
+        total_loss_pinn = total_loss_pinn + loss_pinn
+
+    total_loss_data = total_loss_data / num_batches
+    total_loss_pinn = total_loss_pinn / num_batches
+    return total_loss_data, total_loss_pinn
+
+if __name__=='__main__':
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    dataset = DriftwaveDataset()
+
+    network = PINN()
+    network.to(device)
+
+    T = 100
+    Ln = 0.01
+    v = - 2*pi*T/Ln
+
+    train_dataloader = DataLoader(dataset=dataset, batch_size=10, shuffle=True, num_workers=7)
+
+    loss_fn = nn.MSELoss()
+    optimizer = torch.optim.Adam(network.parameters(), lr=1e-3)
+
+    for epoch in range(50):
+        print(f"Epoch: {epoch+1}")
+        print("-----------------")
+        loss_data, loss_pinn = train_loop(train_dataloader, network, loss_fn, optimizer, 10, v)
+        print(f"Data loss: {loss_data}, PINN loss: {loss_pinn}")
+
+    torch.save(network.state_dict(), 'weights.pt')
+
